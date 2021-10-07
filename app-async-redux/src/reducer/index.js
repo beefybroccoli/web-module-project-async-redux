3c602f23da61077238d1609724179f0d0cbb99e4
beefybroccoli <tedkimemail@yahoo.com>
2021-10-07 00:31:01 -0700

    no comment

---
+++ app-async-redux/src/reducer/index.js
@@ -0,0 +1,18 @@
+const initialState = {
+  loading: false,
+  data: null,
+  isFetching: false,
+  error: null,
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    /* case - fetch start */
+
+    /* case - fetch success */
+
+    /* case - fetch fail */
+    default:
+      return state;
+  }
+};
